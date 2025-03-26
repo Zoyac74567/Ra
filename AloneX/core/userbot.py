1399eb691a5f4ded67b561aad7467fcd3e1a0eac
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2025-03-26 16:21:46 +0530

    ɴᴇᴡ-ᴜᴘᴅᴀᴛᴇ-ʙʏ-ᴀʟᴏɴᴇ-ᴄᴏᴅᴇʀ

--- AloneX/core/userbot.py
+++ AloneX/core/userbot.py
@@ -51,8 +51,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
+                await self.one.join_chat("AloneXSays")
+                await self.one.join_chat("AloneUpdates")
+                await self.one.join_chat("AloneXUpdates")
                 await self.one.join_chat("AloneXBots")
-                await self.one.join_chat("AlonesHeaven")
+                await self.one.join_chat("AloneXSupport")
             except:
                 pass
             assistants.append(1)
@@ -72,8 +75,11 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("AloneXBots")
-                await self.two.join_chat("AlonesHeaven")
+                await self.one.join_chat("AloneXSays")
+                await self.one.join_chat("AloneUpdates")
+                await self.one.join_chat("AloneXUpdates")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("AloneXSupport")
             except:
                 pass
             assistants.append(2)
@@ -93,8 +99,11 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("AloneXBots")
-                await self.three.join_chat("AlonesHeaven")
+                await self.one.join_chat("AloneXSays")
+                await self.one.join_chat("AloneUpdates")
+                await self.one.join_chat("AloneXUpdates")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("AloneXSupport")
             except:
                 pass
             assistants.append(3)
@@ -114,8 +123,11 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("AloneXBots")
-                await self.four.join_chat("AlonesHeaven")
+                await self.one.join_chat("AloneXSays")
+                await self.one.join_chat("AloneUpdates")
+                await self.one.join_chat("AloneXUpdates")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("AloneXSupport")
             except:
                 pass
             assistants.append(4)
@@ -135,8 +147,11 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("AloneXBots")
-                await self.five.join_chat("AlonesHeaven")
+                await self.one.join_chat("AloneXSays")
+                await self.one.join_chat("AloneUpdates")
+                await self.one.join_chat("AloneXUpdates")
+                await self.one.join_chat("AloneXBots")
+                await self.one.join_chat("AloneXSupport")
             except:
                 pass
             assistants.append(5)
