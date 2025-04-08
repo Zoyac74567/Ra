3423e79cdeaac65bf790826ea0b6682074aceabf
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2025-04-08 12:11:43 +0530

    ᴜᴘᴅᴀᴛᴇ-ʙʏ-ᴀʟᴏɴᴇ-ᴄᴏᴅᴇʀ

--- AloneX/utils/inline/start.py
+++ AloneX/utils/inline/start.py
@@ -20,17 +20,30 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
+                        text="˹ 𝐒ᴜᴘᴘᴏʀᴛ ˼", url=config.SUPPORT_CHAT
+                    ),
+            InlineKeyboardButton(
+                        text="˹ 𝐔ᴘᴅᴀᴛᴇs ˼", url=config.SUPPORT_CHANNEL
+                    ),
+        ],
+        [
+            InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
+        [
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
+            InlineKeyboardButton(
+                       text="˹ 𝐎ᴡɴᴇʀ ˼", user_id=config.OWNER_ID
+                    ),
+        ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="alone_op"),
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source"),
-        ],
-        [
-            InlineKeyboardButton("[❄️] 𝐁σт 𝐈иғσ [❄️]", callback_data="bot_info_data"),
+            InlineKeyboardButton("˹ 𝐁σт 𝐈иғσ ˼", callback_data="bot_info_data"),
+      #  ],
+      #  [
+           # InlineKeyboardButton("𓂃❛𝚨Łꪮⲛ𝛆♡𝐒⋏ꪗ𝛅꯭‹//›🍂🌙", url=f"https://t.me/AloneXSays"),
         ],
     ]
     return buttons
