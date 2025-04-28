fe180b6b1d6a076b00a51c7bc9388ba07c34991e
˹ᴧŁꪮɳᴇ ꭙ˼ <131831332+TeamAloneOp@users.noreply.github.com>
2025-04-28 14:43:53 +0530

    ɴᴇᴡ-ᴜᴘᴅᴀᴛᴇ-ʙʏ-ᴀʟᴏɴᴇ-ᴄᴏᴅᴇʀ

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-API_KEY = getenv("API_KEY", "") # youtube song api key, get it from https://t.me/AloneHuVai
+API_KEY = getenv("API_KEY", "30DxNexGenBots7fG9kL") # youtube song api key, get it from https://t.me/AloneHuVai
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 54000))
 
